8662393c82504355d2e3ec1a4a8d531cb33c02e2
juan pablo corzo <juanpablocorzobarrera@gmail.com>
2024-05-13 12:57:29 -0300

    rediseño de register user y company

--- app/home-card/page.jsx
+++ app/home-card/page.jsx
@@ -1,88 +1,76 @@
-"use client";
-
 import React from "react";
-import { useRouter } from "next/navigation";
-import Box from "@mui/material/Box";
-import Grid from "@mui/material/Grid";
-import Paper from "@mui/material/Paper";
-import Typography from "@mui/material/Typography";
-import Button from "@mui/material/Button";
-import CustomToolbar from "@/src/components/Toolbar";
-import Link from "next/link";
+import landing from "../../app/landing.css";
 
-function HomeCard() {
-  const router = useRouter();
+/* ....... */
+import Home from "../../src/components/home/Home";
 
-  const handleUsuarioClick = () => {
-    router.push("/register-user");
-  };
-
-  const handleEmpresaClick = () => {
-    router.push("/register-company");
-  };
-
+function Page() {
   return (
     <>
-      <CustomToolbar />
-      <Box sx={{ flexGrow: 1 }}>
-        <Grid container justifyContent="center" sx={{ marginTop: 5 }}>
-          <Grid item xs={4} sm={4} md={4}>
-            <Link href="/landing">
-              <Button variant="contained" color="error">
-                Volver
-              </Button>
-            </Link>
-          </Grid>
-          <Grid item xs={12} sm={10} md={8} lg={6} xl={4}>
-            <Paper sx={{ padding: 10 }} elevation={8}>
-              <Box sx={{ display: "flex", alignItems: "center", flexDirection: "column" }}>
-                <Typography
-                  variant="h4"
-                  sx={{
-                    fontWeight: "bold",
-                    color: "#344767",
-                    textAlign: "center",
-                    marginBottom: 2,
-                  }}
+      <header>
+        <nav className="z-10 w-full absolute">
+          <div className="container">
+            <div className="flex flex-wrap items-center justify-between py-2 gap-6 md:py-4 md:gap-0 relative">
+              <input
+                aria-hidden="true"
+                type="checkbox"
+                name="toggle_nav"
+                id="toggle_nav"
+                className="hidden peer"
+              />
+              <div className="relative z-20 w-full flex justify-between lg:w-max md:px-0">
+                <a
+                  href="#home"
+                  aria-label="logo"
+                  className="flex space-x-2 items-center"
                 >
-                  Registro Economía del Conocimiento
-                </Typography>
+                  <div aria-hidden="true" className="flex space-x-1">
+                    <div className="h-4 w-4 rounded-full bg-gray-900 dark:bg-white"></div>
+                    <div className="h-6 w-2 bg-primary"></div>
+                  </div>
+                  <span className="text-2xl font-bold text-white dark:text-black">
+                    Registro de economia
+                  </span>
+                </a>
+                <div className="relative flex items-center lg:hidden max-h-10">
+                  <label
+                    role="button"
+                    htmlFor="toggle_nav"
+                    aria-label="humburger"
+                    id="hamburger"
+                    className="relative p-6 -mr-6"
+                  >
+                    <div
+                      aria-hidden="true"
+                      id="line"
+                      className="m-auto h-0.5 w-5 rounded bg-sky-900 dark:bg-gray-300 transition duration-300"
+                    ></div>
+                    <div
+                      aria-hidden="true"
+                      id="line2"
+                      className="m-auto mt-2 h-0.5 w-5 rounded bg-sky-900 dark:bg-gray-300 transition duration-300"
+                    ></div>
+                  </label>
+                </div>
+              </div>
+              <div
+                aria-hidden="true"
+                className="fixed z-10 inset-0 h-screen w-screen bg-white/70 backdrop-blur-2xl origin-bottom scale-y-0 transition duration-500 peer-checked:origin-top peer-checked:scale-y-100 lg:hidden dark:bg-gray-900/70"
+              ></div>
+              <div
+                className="flex-col z-20 flex-wrap gap-6 p-8 rounded-3xl border border-gray-100 bg-white shadow-2xl shadow-gray-600/10 justify-end w-full invisible opacity-0 translate-y-1  absolute top-full left-0 transition-all duration-300 scale-95 origin-top 
+                            lg:relative lg:scale-100 lg:peer-checked:translate-y-0 lg:translate-y-0 lg:flex lg:flex-row lg:items-center lg:gap-0 lg:p-0 lg:bg-transparent lg:w-7/12 lg:visible lg:opacity-100 lg:border-none
+                            peer-checked:scale-100 peer-checked:opacity-100 peer-checked:visible lg:shadow-none 
+                            dark:shadow-none dark:bg-gray-800 dark:border-gray-700"
+              ></div>
+            </div>
+          </div>
+        </nav>
+      </header>
 
-                <Typography sx={{ padding: 2 }}>
-                  Llena el formulario, completa el registro y comenzarás a vivir tu
-                  futuro. Nuestro objetivo es incrementar tus oportunidades
-                  laborales, acompañarte con capacitaciones 100% gratis y hacer tus
-                  logros y habilidades visibles en bootcamps o búsquedas laborales
-                  en las empresas líderes de base tecnológica asociadas al
-                  PoloTecnológico La Rioja.
-                </Typography>
-
-                {/* btn empresa */}
-                <Button
-                  sx={{ bgcolor: "#8B322C", marginTop: 3 }}
-                  variant="contained"
-                  color="error"
-                  onClick={handleEmpresaClick}
-                >
-                  Registrarme como empresa
-                </Button>
-
-                {/* btn personas comunes */}
-                <Button
-                  sx={{ bgcolor: "#8B322C", marginTop: 3 }}
-                  variant="contained"
-                  color="error"
-                  onClick={handleUsuarioClick}
-                >
-                  Registrarme como usuario
-                </Button>
-              </Box>
-            </Paper>
-          </Grid>
-        </Grid>
-      </Box>
+      <Home />
     </>
   );
 }
 
-export default HomeCard;
+export default Page;
---
+++ src/components/home/Home.jsx
@@ -0,0 +1,56 @@
+import Link from "next/link";
+
+function Home() {
+  return (
+    <div className="relative" id="home">
+      <div
+        aria-hidden="true"
+        className="absolute inset-0 grid grid-cols-2 -space-x-52 opacity-40 dark:opacity-20"
+      >
+        <div className="blur-[106px] h-56 bg-gradient-to-br from-primary to-purple-400 dark:from-blue-700"></div>
+        <div className="blur-[106px] h-32 bg-gradient-to-r from-cyan-400 to-sky-300 dark:to-indigo-600"></div>
+      </div>
+      <div className="Container">
+        <div className="relative pt-36 ml-auto">
+          <div className="lg:w-2/3 text-center mx-auto">
+            <Link
+              href="/landing"
+              className="relative flex h-11 w-full items-center justify-center px-6 before:absolute before:inset-0 before:rounded-full before:bg-primary before:transition before:duration-300 hover:before:scale-105 active:duration-75 active:before:scale-95 sm:w-max"
+            >
+              <span className="relative text-base font-semibold text-white">
+                Vovler
+              </span>
+            </Link>
+            <h2 className="mt-8 text-gray-700 dark:text-gray-300">
+              completa el registro y comenzarás a vivir tu futuro. Nuestro
+              objetivo es incrementar tus oportunidades laborales, acompañarte
+              con capacitaciones 100% gratis y hacer tus logros y habilidades
+              visibles en bootcamps o búsquedas laborales en las empresas
+              líderes de base tecnológica asociadas al PoloTecnológico La Rioja.
+            </h2>
+            <div className="mt-16 flex flex-wrap justify-center gap-y-4 gap-x-6">
+              <Link
+                href="/register-user    "
+                className="relative flex h-11 w-full items-center justify-center px-6 before:absolute before:inset-0 before:rounded-full before:bg-primary before:transition before:duration-300 hover:before:scale-105 active:duration-75 active:before:scale-95 sm:w-max"
+              >
+                <span className="relative text-base font-semibold text-white">
+                  Registro como cliente
+                </span>
+              </Link>
+              <Link
+                href="register-company"
+                className="relative flex h-11 w-full items-center justify-center px-6 before:absolute before:inset-0 before:rounded-full before:bg-primary before:transition before:duration-300 hover:before:scale-105 active:duration-75 active:before:scale-95 sm:w-max"
+              >
+                <span className="relative text-base font-semibold text-white">
+                  Registro como empresa
+                </span>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
