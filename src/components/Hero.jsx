bf3f7748cf1aa58097a4139cd1b8bce30fb8e920
juan corzo barrera <juanpablocorzobarrera@gmail.com>
2024-05-22 04:06:24 -0300

    login ready

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 function Hero() {
   return (
     <div className="relative" id="home">
@@ -25,14 +27,14 @@ function Hero() {
               líderes de base tecnológica asociadas al PoloTecnológico La Rioja.
             </p>
             <div className="mt-16 flex flex-wrap justify-center gap-y-4 gap-x-6">
-              <a
-                href="#"
+              <Link
+                href="/login-user"
                 className="relative flex h-11 w-full items-center justify-center px-6 before:absolute before:inset-0 before:rounded-full before:bg-primary before:transition before:duration-300 hover:before:scale-105 active:duration-75 active:before:scale-95 sm:w-max"
               >
                 <span className="relative text-base font-semibold text-white">
-                  Registro
+                  Iniciar sesion
                 </span>
-              </a>
+              </Link>
               <a
                 href="#"
                 className="relative flex h-11 w-full items-center justify-center px-6 before:absolute before:inset-0 before:rounded-full before:border before:border-transparent before:bg-primary/10 before:bg-gradient-to-b before:transition before:duration-300 hover:before:scale-105 active:duration-75 active:before:scale-95 dark:before:border-gray-700 dark:before:bg-gray-800 sm:w-max"
